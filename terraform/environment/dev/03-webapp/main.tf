12308ebf670c2b7cf9b7cf15a5bfe25a9b7ef503
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-10-09 01:14:30 +0300

    Update 03-webapp

--- terraform/environment/dev/03-webapp/main.tf
+++ terraform/environment/dev/03-webapp/main.tf
@@ -6,13 +6,13 @@ module "webapp" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = "0.0.0.0/0"
         },
         {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = "0.0.0.0/0"
 
         }
     ]
--- terraform/modules/03-webapp/variables.tf
+++ terraform/modules/03-webapp/variables.tf
@@ -7,7 +7,7 @@ variable "sg_ingress_rules" {
     from_port = number
     to_port = number
     protocol = string
-    cidr_blocks = any
+    cidr_blocks = string
   }))
 
   default = [
@@ -15,13 +15,13 @@ variable "sg_ingress_rules" {
       from_port = 80
       to_port = 80
       protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = "0.0.0.0/0"
     },
     {
       from_port = 22
       to_port = 22
       protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = "0.0.0.0/0"
 
     }
   ]
