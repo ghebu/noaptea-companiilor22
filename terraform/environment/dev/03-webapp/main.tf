e79c4349f9aa88daf3bfd6ca383d5243436781b2
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-10-09 01:11:24 +0300

    Update 03-webapp

--- terraform/environment/dev/03-webapp/main.tf
+++ terraform/environment/dev/03-webapp/main.tf
@@ -2,18 +2,18 @@ module "webapp" {
     source = "../../../modules/03-webapp"
     name_prefix = "noaptea-companiilor"
     sg_ingress_rules = [
-    {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = "0.0.0.0/0"
+        {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        },
+        {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
 
-    }
-  ]
+        }
+    ]
 }
--- terraform/modules/03-webapp/variables.tf
+++ terraform/modules/03-webapp/variables.tf
@@ -15,13 +15,13 @@ variable "sg_ingress_rules" {
       from_port = 80
       to_port = 80
       protocol = "tcp"
-      cidr_blocks = "0.0.0.0/0"
+      cidr_blocks = ["0.0.0.0/0"]
     },
     {
       from_port = 22
       to_port = 22
       protocol = "tcp"
-      cidr_blocks = "0.0.0.0/0"
+      cidr_blocks = ["0.0.0.0/0"]
 
     }
   ]
