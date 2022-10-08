cb45acdd55ae572af1827f8908558c74d93131dd
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-10-09 00:34:53 +0300

    Draft for 3rd TF module

--- terraform/modules/03-webapp/launch_template.tf
+++ terraform/modules/03-webapp/launch_template.tf
@@ -1,24 +1,3 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  owners      = ["137112412989"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-data "aws_vpc" "main" {
-    filter {
-        name = 	"tag:Name"
-        value = "noaptea-companiilor-vpc"
-    }
-}
 
 resource "aws_security_group" "sg" {
   name        = "${var.name_prefix}-sg"
@@ -84,7 +63,7 @@ resource "aws_launch_template" "foo" {
     }
   }
 
-  user_data = filebase64("${path.module}/user-data.sh" )
+  user_data = filebase64("${path.module}/user-data.sh")
 }
 
 
