2f52bc53eb18337bd56c815b74d0cfb66009c817
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-10-09 00:31:27 +0300

    Draft for 3rd TF module

--- terraform/environment/dev/03-webapp/main.tf
+++ terraform/environment/dev/03-webapp/main.tf
@@ -1,3 +1,8 @@
 module "webapp" { 
     source = "../../../modules/03-webapp"
+    sg_ingress_rules = {
+        from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = "0.0.0.0/0",
+        from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = "0.0.0.0/0"
+
+    }
 }
--- terraform/modules/03-webapp/variables.tf
+++ terraform/modules/03-webapp/variables.tf
@@ -2,3 +2,7 @@ variable "instance_profile_name" {
     default = "SSMRole4EC2"
 }
 
+variable "sg_ingress_rules" { 
+    map = {
+        from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = "0.0.0.0/0"}
+}
