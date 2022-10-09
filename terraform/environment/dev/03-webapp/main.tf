6989ca25f4331912dc5b4afdc8ce6b4c157867b1
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-10-09 11:53:18 +0300

    Update 03-webapp

--- terraform/environment/dev/03-webapp/main.tf
+++ terraform/environment/dev/03-webapp/main.tf
@@ -1,7 +1,7 @@
 module "webapp" { 
     source = "../../../modules/03-webapp"
     name_prefix = "noaptea-companiilor"
-    network_remote_state_bucket = remote_state_bucket
+    network_remote_state_bucket = var.remote_state_bucket
     # sg_ingress_rules = [
     #     {
     #         from_port = 80, to_port = 80, protocol = "tcp"
