ae78525277c0a66cb1af3a159de88e2ce75421d8
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-10-09 10:42:01 +0300

    Update 03-webapp

--- terraform/modules/03-webapp/variables.tf
+++ terraform/modules/03-webapp/variables.tf
@@ -2,27 +2,27 @@ variable "instance_profile_name" {
     default = "SSMRole4EC2"
 }
 
-variable "sg_ingress_rules" {
-  type = list(object({
-    from_port = number
-    to_port = number
-    protocol = string
-  }))
+# variable "sg_ingress_rules" {
+#   type = list(object({
+#     from_port = number
+#     to_port = number
+#     protocol = string
+#   }))
 
-  default = [
-    {
-      from_port = 80
-      to_port = 80
-      protocol = "tcp"
+#   default = [
+#     {
+#       from_port = 80
+#       to_port = 80
+#       protocol = "tcp"
       
-    },
-    {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-    }
-  ]
-}
+#     },
+#     {
+#       from_port = 22
+#       to_port = 22
+#       protocol = "tcp"
+#     }
+#   ]
+# }
 
 variable "name_prefix" { 
     default = "noaptea-companiilor"
