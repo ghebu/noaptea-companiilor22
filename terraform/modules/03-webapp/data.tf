8873a689a45f82a1536ef0fd556b2711cad3bf0c
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-10-09 11:13:42 +0300

    Update 03-webapp

--- terraform/modules/03-webapp/data.tf
+++ terraform/modules/03-webapp/data.tf
@@ -1,17 +1,17 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  owners      = ["137112412989"]
+# data "aws_ami" "ami" {
+#   most_recent = true
+#   owners      = ["137112412989"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-kernel-5.10-hvm-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
 # data "aws_vpc" "main" {
 #     filter {
