f68012e4d41cdef6e8256026c5d3c04eb9b16649
Marius Ghebuta <Marius.Ghebuta@partner.bmw.de>
2022-10-09 10:55:32 +0300

    Update 03-webapp

--- terraform/modules/03-webapp/launch_template.tf
+++ terraform/modules/03-webapp/launch_template.tf
@@ -7,12 +7,10 @@ resource "aws_security_group" "sg" {
     dynamic "ingress" {
         for_each = var.sg_ingress_rules
         content {
-            ingress {
                 from_port        = ingress.value.from_port
                 to_port          = ingress.value.to_port
                 protocol         = ingress.value.protocol
                 cidr_blocks      = ["0.0.0.0/0"]
-            }
         }
     }
 
